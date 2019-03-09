2a4dd45b9f3e51de852ca871efccb8354e45768a
FinalOne <lakhanrathi65@gmail.com>
2019-03-10 01:42:50 +0530

    UI of Teachers My Class page

---
+++ app/src/main/java/com/vkarmaedu/vkarma/fragment/TeacherMyClassFragment.java
@@ -0,0 +1,30 @@
+package com.vkarmaedu.vkarma.fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vkarmaedu.vkarma.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TeacherMyClassFragment extends Fragment {
+
+
+    public TeacherMyClassFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_teacher_my_class, container, false);
+    }
+
+}
